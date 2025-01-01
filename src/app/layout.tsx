fc5bc3be0fde425f918c073483e10cc6837422bd
adithya2152 <adithyabharadwaj15@gmail.com>
2025-01-01 11:02:55 +0530

    pwa-fix-trial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/global.css";
+// import InstallButton from "@/components/installButton";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,14 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      {/* Add DOCTYPE */}
-      
       <html lang="en">
         <head>
+          {/* Add PWA related meta tags */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          
+          {/* Add Apple touch icon */}
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
           {/* Link to manifest.json */}
           <link rel="manifest" href="/manifest.json" />
         </head>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {/* <InstallButton /> */}
           {children}
         </body>
       </html>
